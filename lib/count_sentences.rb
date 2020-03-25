9c018fecdf704d4c64d95f5bb40bf4a7ede06edc
SincerelyBrittany <BrittanyRussell1993@gmail.com>
2020-03-25 16:44:28 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
-  end
+     self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+   end
+  # def count_sentences
+  #   the_count = self.split
+  #   if the_count.count == 0
+  #     0
+  #   else the_count.count > 0
+  #     the_count.count
+  #   end
+  # end
 end
